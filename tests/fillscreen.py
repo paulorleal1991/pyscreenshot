90390e22bce271b641b4406be9f3d63d9086a909
ponty <ponty@home>
2020-01-24 12:28:17 +0100

    x

--- tests/fillscreen.py
+++ tests/fillscreen.py
@@ -48,6 +48,24 @@ def init(size=None):
     # pygame.mixer.quit()  # to avoid 100% CPU load
     pygame.mouse.set_visible(0)
 
+    if size:
+        disp = pygame.display.set_mode(size)
+    else:
+        disp = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+    rectsize = 50
+    w, h = pygame.display.get_surface().get_size()
+    i = 0
+    for x in range(0, w, rectsize):
+        for y in range(0, h, rectsize):
+            r = x * 255 / w
+            g = y * 255 / h
+            b = (i % 5) * 255 / 5
+            pygame.draw.rect(disp, (r, g, b), (x, y, rectsize, rectsize))
+            i += 1
+
+    pygame.display.update()
+
 
 @entrypoint
 def main(size=None):
