7ed727cc6913f4ecd04e2a93233d8cf070ac31fd
ponty <ponty@home>
2020-01-24 12:37:31 +0100

    x

--- tests/fillscreen.py
+++ tests/fillscreen.py
@@ -15,15 +15,15 @@ def init(size=None):
         disp = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
     rectsize = 50
-    w, h = pygame.display.get_surface().get_size()
-    i = 0
-    for x in range(0, w, rectsize):
-        for y in range(0, h, rectsize):
-            r = x * 255 / w
-            g = y * 255 / h
-            b = (i % 5) * 255 / 5
-            pygame.draw.rect(disp, (r, g, b), (x, y, rectsize, rectsize))
-            i += 1
+    # w, h = pygame.display.get_surface().get_size()
+    # i = 0
+    # for x in range(0, w, rectsize):
+    #     for y in range(0, h, rectsize):
+    #         r = x * 255 / w
+    #         g = y * 255 / h
+    #         b = (i % 5) * 255 / 5
+    #         pygame.draw.rect(disp, (r, g, b), (x, y, rectsize, rectsize))
+    #         i += 1
 
     pygame.display.update()
 
@@ -65,6 +65,8 @@ def init(size=None):
             i += 1
 
     pygame.display.update()
+
+    sleep(1)
 
 
 @entrypoint
