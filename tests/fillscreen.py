d0a2ab399a13e5cbffe998ed8528ed7f8384e596
ponty <ponty@home>
2020-01-24 13:59:04 +0100

    9

--- tests/fillscreen.py
+++ tests/fillscreen.py
@@ -8,51 +8,13 @@ def init(size=None):
     rectsize = 50
 
     pygame.display.init()
-    # pygame.mixer.quit()  # to avoid 100% CPU load
     pygame.mouse.set_visible(0)
-
-    # if size:
-    #     disp = pygame.display.set_mode(size)
-    # else:
-    #     disp = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-
-    sleep(1)
-    # w, h = pygame.display.get_surface().get_size()
-    # i = 0
-    # for x in range(0, w, rectsize):
-    #     for y in range(0, h, rectsize):
-    #         r = x * 255 / w
-    #         g = y * 255 / h
-    #         b = (i % 5) * 255 / 5
-    #         pygame.draw.rect(disp, (r, g, b), (x, y, rectsize, rectsize))
-    #         i += 1
-
-    # pygame.display.update()
-
-    # TODO: how to wait for displayed image?
-
-    # wait for displaying image
-    # pygame.display.update()
-    # pygame.display.update()
-    # pygame.display.update()
-    # pygame.display.update()
-    # pygame.display.update()
-    # pygame.display.update()
-    # pygame.display.update()
-    # sleep(1)
-
-    # print(pygame.display.Info())
-    # print(pygame.display.list_modes())
-
-    #########################################
-    # pygame.display.init()
-    # pygame.mixer.quit()  # to avoid 100% CPU load
-    # pygame.mouse.set_visible(0)
 
     if size:
         disp = pygame.display.set_mode(size)
     else:
         disp = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        disp = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)  # TODO: for travis
 
     w, h = pygame.display.get_surface().get_size()
     i = 0
