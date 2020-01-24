80dd12539654659eb3953fa52b148d4f9b583bc6
ponty <ponty@home>
2020-01-24 12:41:37 +0100

    x

--- tests/fillscreen.py
+++ tests/fillscreen.py
@@ -5,6 +5,8 @@ from entrypoint2 import entrypoint
 
 
 def init(size=None):
+    rectsize = 50
+
     pygame.display.init()
     # pygame.mixer.quit()  # to avoid 100% CPU load
     pygame.mouse.set_visible(0)
@@ -14,16 +16,15 @@ def init(size=None):
     else:
         disp = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-    rectsize = 50
-    # w, h = pygame.display.get_surface().get_size()
-    # i = 0
-    # for x in range(0, w, rectsize):
-    #     for y in range(0, h, rectsize):
-    #         r = x * 255 / w
-    #         g = y * 255 / h
-    #         b = (i % 5) * 255 / 5
-    #         pygame.draw.rect(disp, (r, g, b), (x, y, rectsize, rectsize))
-    #         i += 1
+    w, h = pygame.display.get_surface().get_size()
+    i = 0
+    for x in range(0, w, rectsize):
+        for y in range(0, h, rectsize):
+            r = x * 255 / w
+            g = y * 255 / h
+            b = (i % 5) * 255 / 5
+            pygame.draw.rect(disp, (r, g, b), (x, y, rectsize, rectsize))
+            i += 1
 
     pygame.display.update()
 
@@ -46,15 +47,14 @@ def init(size=None):
     #########################################
     pygame.display.init()
     # pygame.mixer.quit()  # to avoid 100% CPU load
-    pygame.mouse.set_visible(0)
+    # pygame.mouse.set_visible(0)
 
     if size:
         disp = pygame.display.set_mode(size)
     else:
         disp = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-    rectsize = 50
-    w, h = pygame.display.get_surface().get_size()
+    # w, h = pygame.display.get_surface().get_size()
     i = 0
     for x in range(0, w, rectsize):
         for y in range(0, h, rectsize):
